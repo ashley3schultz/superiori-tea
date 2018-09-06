a4a8e2cb5a0eae9b6e136c6da0ff4cdc680d2dcf
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-05 20:51:38 -0700

    Add tree component

---
+++ src/components/Tree.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Tree = props => {
+    return (
+        <div>
+            <img src='.../public/images/stem-b.png' alt='stem'/>
+            <img src='.../public/images/leaf-a1.png' alt='leaf'/>
+            <img src='.../public/images/leaf-a2.png' alt='leaf' />
+            <img src='.../public/images/leaf-a3.png' alt='leaf' />
+            <img src='.../public/images/leaf-a4.png' alt='leaf' />
+            <img src='.../public/images/leaf-a5.png' alt='leaf' />
+            <img src='.../public/images/leaf-a6.png' alt='leaf' />
+            <img src='.../public/images/leaf-a7.png' alt='leaf' />
+        </div>
+    )
+} 
+export default Tree
