0d86082fefdf3de5d46a212cd8c3b6349f2f0ba8
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-11 09:54:26 -0700

    Update game reducer

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,9 +1,13 @@
-// import {FETCH_TOP_SCORES} from '../actions/actionTypes';
+// import {FETCH_TOP_SCORES} from '../actions/fetchTopScoresAction';
 
-export default function game(state = [], action) {
+export default function game(
+  state = {
+    game: '',
+  }, action) {
   switch (action.type) {
-    case FETCH_TOP_SCORES:
-      return state;
+    // case FETCH_TOP_SCORES:
+    //   return state;
+
     default:
       return state;
   }
