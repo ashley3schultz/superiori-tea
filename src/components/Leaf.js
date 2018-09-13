ffff2cac495b009f9a9c54022df1541a1513cea0
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-12 21:30:38 -0700

    Streamline Leaf component

--- src/components/Leaf.js
+++ src/components/Leaf.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Leaf = props => {
       return (
-        <div className={props.type + '-' + props.align}> 
+        <div>
             <img 
                 onClick={props.collectLeaf} 
-                className={props.type}
-                align={props.align}
                 src={require('./images/' + props.leaf + '.png')} 
                 alt={props.leaf} 
                 id={props.stem + props.leaf.replace('leaf-a', '').replace('leaf-b', '')}
