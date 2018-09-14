678670873d0cd535253291d9efdc51ea02b0087a
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-13 18:59:29 -0700

    Debug changes to get game scoring functioning

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -6,80 +6,78 @@ import {emptyBasket, trees, rules} from '../components/Data'
 class Game extends Component {
 
     state = {
-        msg: <button onClick={this.playGame}>Start</button>,
-        level: 0,
-        score: [],
+        msg: undefined,
+        level: 4,
+        scores: [],
+        score: 0,
         trees: trees.slice(),
         basket: emptyBasket,
-        time: 15
+        time: 15,
+        rules: rules
     }
-
-    // startTimer = () => {
-    //   this.timer = setInterval(() => this.setState({time: this.state.time - 1}), 1000);
-    //   if (this.state.time === 0) {
-    //     clearInterval(this.timer)
-    //     this.setState({level: this.state.level + 1})
-    //     this.setState({score: this.state.score.concat(addScore(this.state.basket))})
-    //     this.setState({trees: trees.slice()})
-    //     this.setState({basket: emptyBasket})
-    //     this.setState({time: 15})
-    //   }
-    // }
 
     playGame = () => {
         if (this.state.time > 0) { //ajusts timer
-            this.setState({msg: rules[this.state.level].tips}) if this.state.time === 15
+            this.setState({msg: rules[this.state.level].tips})
             this.setState({time: this.state.time - 1})
-            setTimeout(startTimer, 1000);
+            this.setState({score: this.addScore(this.state.basket)})
+            setTimeout(this.playGame, 1000);
         } else if (this.state.time === 0) {
             if (this.state.level < 4) { //sets state for next level
+                this.setState({msg: undefined})
                 this.setState({level:  this.state.level + 1})
-                this.setState({score: this.state.score.concat(addScore(this.state.basket))})
+                this.setState({scores: this.state.scores.concat(this.state.score)})
                 this.setState({trees: trees.slice()})
                 this.setState({basket: emptyBasket})
                 this.setState({time: 15})
+                this.setState({score: 0})
+                console.log(this.state)
             } else { //saves game!
-                this.setState({msg: <button onClick={this.playGame}>Play Again?</button>})
-                this.setState({score: this.state.score.concat(addScore(this.state.basket))})
-                this.saveGame
+                this.setState({msg: undefined})
+                this.setState({scores: this.state.scores.concat(this.state.score)})
+                this.setState({score: (this.state.scores[0] + this.state.scores[1] + this.state.scores[2] + this.state.scores[3] + this.state.scores[4])})
+                this.saveGame()
+                this.resetGame()
             }
         }
     }
 
-    replayGame = () => {
+    resetGame = () => {
         this.setState({level: 0})
         this.setState({score: []})
         this.setState({trees: trees.slice()})
         this.setState({basket: emptyBasket})
         this.setState({time: 15})
+        this.setState({msg: undefined})
     }
 
     saveGame = () => {
-      alert('game saved')
+      console.log('game saved')
     }
 
     addScore = (basket) => {
-      var purity = basket.pureCultivar / basket.total
-      var quality = basket.qualityLeaves / basket.pureCultivar
-      var outOf = basket.pureCultivar / rules[this.state.level].outOf
-      return (purity + quality + outOf)
+      var purity = basket.pureCultivar / basket.total * 100 || 0
+      var quality = basket.qualityLeaves / basket.pureCultivar * 100 || 0
+      var outOf = basket.pureCultivar / this.state.rules[this.state.level].outOf *100 || 0
+      return (Math.floor(purity) + Math.floor(quality) + Math.floor(outOf))
     }
 
-    collectLeaf = (event) => {
-      const leaf = event.target.getAttribute('id')split('*')
-      if (leaf.length < 4 && this.state.time > 0) {
-        this.setState({basket.total: this.state.basket.total + 1})
-        var array = this.state.trees.leaves
-        var index = array.indexOf(event.target.value)
-        delete array[index];
-        if (leaf[0] === rules[this.state.level].purity) {
-          this.setState({basket.pureCultivar: this.state.basket.pureCultivar + 1})
-        }
-        if (leaf[1] =< rules[this.state.level].quality) {
-          this.setState({basket.qualityLeaves: this.state.basket.qualityLeaves + 1})
-        }
-      }
-    }
+    // collectLeaf = (event) => {
+    //   const leaf = event.target.getAttribute('id')
+    //   // split('*')
+    //   if (leaf.length < 4 && this.state.time > 0) {
+    //     this.setState({basket.total: this.state.basket.total + 1})
+    //     var array = this.state.trees.leaves
+    //     var index = array.indexOf(event.target.value)
+    //     delete array[index];
+    //     if (leaf[0] === rules[this.state.level].purity) {
+    //       this.setState({basket.pureCultivar: this.state.basket.pureCultivar + 1})
+    //     }
+    //     if (leaf[1] =< rules[this.state.level].quality) {
+    //       this.setState({basket.qualityLeaves: this.state.basket.qualityLeaves + 1})
+    //     }
+    //   }
+    // }
 
   render() {
     return (
@@ -87,7 +85,8 @@ class Game extends Component {
         <h3>Level {this.state.level + 1}</h3>
         <p>Basket: {this.state.basket.total}</p>
         <p>Time: {this.state.time}</p>
-        {this.state.msg}
+        <p>Score: {this.state.score}</p>
+        {this.state.msg || <button onClick={this.playGame}>Start</button>}
         <Trees trees={this.state.trees} collectLeaf={this.collectLeaf}/>
       </div>
     );
@@ -96,7 +95,7 @@ class Game extends Component {
 
 const mapStateToProps = state => {
   return {
-    trees: this.state.trees
+    trees: this.state
   }
 }
 
