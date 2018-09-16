6d3bb9de62798720522b6ec2ce7b71a03046dffe
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-16 16:37:21 -0700

    By pass rootReducer for testing purposes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './reducers/rootReducer'
+import gameReducer from './reducers/gameReducer'
 import thunk from 'redux-thunk';
 
-let store = createStore(rootReducer,
+let store = createStore(gameReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
