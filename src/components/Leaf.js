cc11afc3d495a282b5d5b2111498ff8d4d6104c9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-09-17 22:48:16 +0000

    Update leaf images

--- src/components/Leaf.js
+++ src/components/Leaf.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const Leaf = props => {
+      const name = props.leaf.name
+      const num = name.replace('a', '').replace('b', '')
       return (
         <div className="leaf">
             <img
                 onClick={props.handleLeafClick}
                 src={require('./images/' + props.leaf.name + '.png')}
                 alt={props.leaf.name}
-                id={props.stem + '*' + props.leaf.name.replace('leaf-a', '').replace('leaf-b', '') + '*' + props.leaf.id}
-                className={(props.leaf.name.replace('leaf-a', '').replace('leaf-b', '') < 3) ? 'bud' : 'leaf'}
+                id={props.stem + '*' + num + '*' + props.leaf.id}
+                className={(num < 3) ? 'bud' : 'leaf'}
             />
         </div>
     )
