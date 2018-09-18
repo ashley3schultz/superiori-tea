c796e39cf9ff39ba4bb70526a8424040dfa95e54
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-17 20:20:11 -0700

    Update className for leaf to hide image once clicked

--- src/components/Leaf.js
+++ src/components/Leaf.js
@@ -4,13 +4,13 @@ const Leaf = props => {
       const name = props.leaf.name
       const num = name.replace('a', '').replace('b', '')
       return (
-        <div className="leaf">
+        <div className='leaf'>
             <img
                 onClick={props.handleLeafClick}
                 src={require('./images/' + props.leaf.name + '.png')}
                 alt={props.leaf.name}
-                id={props.stem + '*' + num + '*' + props.leaf.id}
-                className={(num < 3) ? 'bud' : 'leaf'}
+                id={props.stem + '*' + num + '*' + props.leaf.id + '*' + props.leaf.status}
+                className={(num < 3) ? 'bud ' + props.leaf.status : 'leaf ' + props.leaf.status }
             />
         </div>
     )
