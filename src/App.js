53c3ce7592ccca2871acff1e5f11b247609fbf1b
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-18 09:16:06 -0700

    Add side bars for white space

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div>
+      <div className="LeftSide">
+      </div>
       <div className="App">
         <header className="App-header">
           <h1 className="Superiori-Tea">Superiori-Tea</h1>
         </header>
         <Game/>
+      </div>
+      <div className="RightSide">
+      </div>
       </div>
     );
   }
