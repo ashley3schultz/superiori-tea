2a45b65ae6ee5203faf43725703ef0de7e8471fa
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 10:37:42 -0700

    Add veiw score components for added routes

---
+++ src/components/MyScores.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyScores = props => {
+      return (
+        <div>
+            <h1>My Scores</h1>
+            <ol>
+                <li>Date: 1/15/2018 | Score: 1265</li>
+                <li>Date: 1/20/2018 | Score: 1149</li>
+                <li>Date: 2/11/2018 | Score: 1460</li>
+            </ol>
+            
+        </div>
+    )
+}
+export default MyScores
