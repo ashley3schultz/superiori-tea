9c896bce676631b92010127b46d525b0db171831
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 10:30:27 -0700

    Add react-router to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import Game from './containers/GameContainer'
+import GameContainer from './containers/GameContainer'
 import './App.css';
 import Backgound from './components/Background';
 import { connect } from 'react-redux'
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import TopScores from "./components/TopScores"
+import MyScores from "./components/MyScores"
 
 class App extends Component {
   render() {
@@ -11,10 +15,19 @@ class App extends Component {
       <div className="App">
         <Backgound playing={this.props.playing}/>
         <div className="dirt"></div>
-        <header className="App-header">
-          <h1 className="Superiori-Tea">Superiori-Tea</h1>
-        </header>
-        <Game/>
+        <BrowserRouter>
+          <div className="App">
+            <NavBar/>
+            <header className="App-header">
+              <h1 className="Superiori-Tea">Superiori-Tea</h1>
+            </header>
+            <Switch>
+              <Route path="/" component={GameContainer} exact/>
+              <Route path="/my-scores" component={MyScores}/>
+              <Route path="/top-scores" component={TopScores}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
@@ -22,7 +35,8 @@ class App extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    playing: state.playing
+    playing: state.playing,
+    user: state.user
   }
 }
 
