b19d6c2dfea0f04e382e40160af91d22310972ca
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 14:59:19 -0700

    Update leader board

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const LeaderBoard = props => {
+
       return (
         <div className='LeaderBoard'>
-            <h3>{props.game.name}: {props.game.score}</h3>
+            <h3>{props.game.user}: {props.game.score}</h3>
         </div>
     )
 }
