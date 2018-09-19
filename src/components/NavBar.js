4a9cf6fe9c424f24500310e82183e9f09177fe6a
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 15:00:05 -0700

    Update text for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom"
 const NavBar = props => {
       return (
         <div className="navbar">
-            <NavLink to='/'>Play Game</NavLink> |
-            | <NavLink to='/my-scores'>My Scores</NavLink> |
-            | <NavLink to='/top-scores'>Top Scores</NavLink>
+            <NavLink to='/'>Play Game</NavLink>
+            <NavLink to='/my-scores'>My Scores</NavLink>
+            <NavLink to='/top-scores'>Top Scores</NavLink>
         </div>
     )
 }
