b8751479b5ca326770a6d9034275dd009ee6d27a
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 10:37:10 -0700

    Update user input component to be statefull

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-const Trees = props => {
+class UserInput extends Component {
+    state = {
+        input: ''
+    }
 
-    return (
-        <div className='UserInput'>
-            <input
-              type='text'
-              value={props.user}
-              onChange={props.handleInput}
-              placeholder='Enter Your Name'
-            />
-        </div>
-    )
+    handleInput = (event) => {
+        const input = event.target.value
+        this.setState({input: input})
+    } 
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.updateUser(this.state.input)
+        this.setState({input: ''})
+    }
+
+    render() {
+        return (
+            <div className='UserInput'>
+            {console.log('state: ' + this.state.input)}
+            {console.log('User: ' + this.props.user)}
+            <form onSubmit={this.handleSubmit}>
+                <input
+                type='text'
+                value={this.state.input}
+                onChange={this.handleInput}
+                placeholder='Enter Your Name'
+                />
+            </form>
+            </div>
+        )
+    }
 }
-export default Trees
+const mapStateToProps = (state) => {
+    return {
+      user: state.user
+    }
+  }
+  
+  const mapDispatchToProps = dispatch => ({
+    updateUser: input => dispatch({ type: "UPDATE_INPUT", input }),
+  })
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(UserInput)
