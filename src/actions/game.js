840df14563f4991b41ddc45954b18e2e17b3b22a
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 12:04:36 -0700

    update fecth action

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,12 +1,49 @@
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
 
-export const fetchTopScore = (game) => {
-    console.log('action top score')
+export const fetchTopScore = () => {
+    return dispatch => {
+        return fetch(`http://192.168.1.30:3001/api/v1/games.json`)
+        .then(response => response.json())
+        .then(game => dispatch(updateTopScore(game)))
+    }
+}
+
+export const saveGame = (name, score) => {
+    let body = JSON.stringify({game: {name: name, score: score} })
+    return dispatch => {
+        return fetch('http://192.168.1.30:3001/api/v1/games', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: body,
+      }).then(response => response.json())
+      .then(game => dispatch(updateTopScore(game)))
+    }
+}
+
+// export const fetchAllScores = () => {
+//     return dispatch => {
+//         return fetch(`http://192.168.1.30:3001/api/v1/games.json`)
+//         .then(response => response.json())
+//         .then(game => dispatch(updateTopScore(game)))
+//     }
+// }
+
+// export const fetchMyScores = () => {
+//     return dispatch => {
+//         return fetch(`http://192.168.1.30:3001/api/v1/games.json`)
+//         .then(response => response.json())
+//         .then(game => dispatch(updateTopScore(game)))
+//     }
+// }
+
+export const updateTopScore = (game) => {
     return { type: "UPDATE_TOP_SCORE", game: game }
 }
 
 export const updateUser = (input) => {
-  return { type: "UPDATE_INPUT", input: input }
+    return { type: "UPDATE_USER", input: input }
 }
 
 export const startLevel = () => {
