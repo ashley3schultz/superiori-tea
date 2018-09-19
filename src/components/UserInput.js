8f6d348b393d57c4c8df93194916de8f7893a90e
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 12:39:47 -0700

    Update name input form

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import {updateUser} from "../actions/game";
 
 class UserInput extends Component {
     state = {
@@ -20,28 +21,21 @@ class UserInput extends Component {
     render() {
         return (
             <div className='UserInput'>
-            {console.log('state: ' + this.state.input)}
-            {console.log('User: ' + this.props.user)}
-            <form onSubmit={this.handleSubmit}>
-                <input
-                type='text'
-                value={this.state.input}
-                onChange={this.handleInput}
-                placeholder='Enter Your Name'
-                />
-            </form>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                    type='text'
+                    value={this.state.input}
+                    onChange={this.handleInput}
+                    placeholder='Enter Your Name'
+                    />
+                </form>
             </div>
         )
     }
 }
-const mapStateToProps = (state) => {
-    return {
-      user: state.user
-    }
+  
+  const mapDispatchToProps = {
+    updateUser,
   }
   
-  const mapDispatchToProps = dispatch => ({
-    updateUser: input => dispatch({ type: "UPDATE_INPUT", input }),
-  })
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(UserInput)
+  export default connect(null, mapDispatchToProps)(UserInput)
