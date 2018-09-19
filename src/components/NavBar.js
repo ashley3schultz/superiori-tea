77bb0eb64e8a13234bfef5ee075b0a5e2277f73c
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 10:36:36 -0700

    Add nav bar to application

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from "react-router-dom"
+
+const NavBar = props => {
+      return (
+        <div className="navbar">
+            <NavLink to='/'>Play Game</NavLink> |
+            | <NavLink to='/my-scores'>My Scores</NavLink> |
+            | <NavLink to='/top-scores'>Top Scores</NavLink>
+        </div>
+    )
+}
+export default NavBar
