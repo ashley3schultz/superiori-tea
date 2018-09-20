9e5713fbe480f33020e290d55481d637bbacd69f
ashley3schultz <ashleyeschultz@gmail.com>
2018-09-19 21:23:06 -0700

    Add protocols to show game info

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,19 +6,20 @@ const Game = props => {
     const { msg, trees, level, time, playing } = props.data
     const { cultivar, quality, total } = props.data.basket
     const pty = cultivar / total * 100 || 0
-    const qty = quality / cultivar * 100 || 0
+    const qty = quality / total * 100 || 0
     const qta = quality / rules[level].outOf * 100 || 0
       return (
         <div>
             <div className='timer'><h5>Time</h5><h3>{time}</h3></div>
-            <h3>
+            <h3 className='level'>
                 Level {level + 1}
             </h3>
+            {(playing) ?
             <p>    
                 Purity: {Math.floor(pty)}%
                 ~ Quality: {Math.floor(qty)}% ~
                 Quota: {Math.floor(qta)}%
-            </p>
+            </p> : null}
             {msg || <button onClick={props.playGame}>Start</button>}
             {(playing) ? 
             <Trees trees={trees} handleLeafClick={props.handleLeafClick}/> :
