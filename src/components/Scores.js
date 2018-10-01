57b5944d90f002c03cd1953fe24450db94d84517
ashley3schultz <ashleyeschultz@gmail.com>
2018-10-01 13:23:24 -0700

    Remove Likes from scores and like buttons

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,21 +1,22 @@
 import React from 'react';
             
 const Scores = props => {
-    const games = props.games.sort((a, b) => b.score - a.score )
       return (
         <div className='scoreboard'>
+        {/* <button className='likeButton' onClick={props.handleSort}>Sort</button> */}
             <h2>{(props.type === 'm') ? 
             "My" : "Top"} Scores</h2>
             {(props.games.length === 0) ? 
                 <p>No games found.</p> : null}
             <ol>
-                {games.map(game => 
+                {props.games.map(game => 
                     <li 
                         key={game.id}>
                         {(props.type === 'm') ? 
                         `${game.created_at.split('T')[0]}: ` : 
                         `${game.user}: `}
-                        {game.score} ~ {game.likes}<button className='likeButton' id={game.id} onClick={props.handleLike}>+</button>
+                        {game.score}
+                         {/* ~ {game.likes}<button className='likeButton' id={game.id} onClick={props.handleLike}>+</button> */}
                     </li>
                 )}
             </ol>
--- src/containers/ScoresContainer.js
+++ src/containers/ScoresContainer.js
@@ -1,61 +1,68 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 import Scores from '../components/Scores'
-// import {fetchScores} from "../actions/game";
-const API_URL = process.env.REACT_APP_API_URL;
+import {fetchScores} from "../actions/game";
+// const API_URL = process.env.REACT_APP_API_URL;
 
 class ScoresContainer extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { games: [] };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { games: [] };
+    // }
 
     componentDidMount() {
-        fetch(`${API_URL}.json`)
-        .then(response => response.json())
-        .then(games => this.setState({games: games}))
-        .catch(error => console.log(error));
+        this.props.fetchScores()
+        // fetch(`${API_URL}.json`)
+        // .then(response => response.json())
+        // .then(games => this.setState({games: games}))
+        // .catch(error => console.log(error));
     }
+
+    // handleSort = () => {
+    //     this.setState(prevState => {
+    //         return { games: prevState.games.sort((a, b) => b.likes - a.likes)}
+    //     })
+    // }
           
-    handleLike = (event) => {
-        const gameid = parseInt(event.target.getAttribute('id'), 1)
-        const gamearray = (this.state.games.length === 0) ? this.props.games : this.state.games
-        const games = gamearray.map(game => {
-            return {
-                id: game.id, 
-                user: game.user,
-                score: game.score, 
-                likes: (game.id === gameid) ? game.likes + 1 : game.likes,
-                created_at: game.created_at
-            }
-        })
-        this.setState({games: games})
+    // handleLike = (event) => {
+    //     const gameid = parseInt(event.target.getAttribute('id'))
+    //     const gamearray = (this.state.games.length === 0) ? this.props.games : this.state.games
+    //     const games = gamearray.map(game => {
+    //         return {
+    //             id: game.id, 
+    //             user: game.user,
+    //             score: game.score, 
+    //             likes: (game.id === gameid) ? game.likes + 1 : game.likes,
+    //             created_at: game.created_at
+    //         }
+    //     })
+    //     this.setState({games: games})
         
-        let body = JSON.stringify({game: {id: gameid} })
-        fetch(`${API_URL}/${gameid}`, {
-              method: 'PATCH',
-              body: body,
-          }).then(response => response.json())
-          .catch(error => console.log(error));
-    }
+    //     let body = JSON.stringify({game: {id: gameid} })
+    //     fetch(`${API_URL}/${gameid}`, {
+    //           method: 'PATCH',
+    //           body: body,
+    //       }).then(response => response.json())
+    //       .catch(error => console.log(error));
+    // }
 
     render() {
         return (
             <div>
-                {(this.state.games.length === 0) ? 'Loading Scores...' :
-                <Scores type='t' games={this.state.games} handleLike={this.handleLike}/>}
+                {(this.props.games.length === 0) ? 'Loading Scores...' :
+                <Scores type='t' games={this.props.games} handleLike={this.handleLike} handleSort={this.handleSort}/>}
             </div>
         )
     }
 }
 
-// const mapStateToProps = (state) => {
-//     return {
-//         games: state.games,
-//         user: state.user
-//     }
-// }
+const mapStateToProps = (state) => {
+    return {
+        games: state.games,
+        user: state.user
+    }
+}
 
-export default ScoresContainer
-// export default connect(mapStateToProps, {fetchScores})(ScoresContainer)
+// export default ScoresContainer
+export default connect(mapStateToProps, {fetchScores})(ScoresContainer)
